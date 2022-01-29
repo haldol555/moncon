eab948794a7b1162e3e70c5ea6016b9f051e09a1
jerry chhun <jc.87@protonmail.com>
2022-01-28 19:26:20 -0600

    testing

--- xmr.sh
+++ xmr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #will dl xmrig, config,
-
+#hello world, added change test
+#HIiiiiii
 curl -o xmrig-6.16.3-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.16.3/xmrig-6.16.3-linux-x64.tar.gz
 tar -xzvf xmrig-6.16.3-linux-x64.tar.gz
